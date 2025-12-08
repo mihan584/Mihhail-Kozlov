c15f71f15a2d6d2e795e8c33e74b9c438cfeb746
Opilane TTHK <opilane@tthk.ee>
2025-12-08 11:40:47 +0200

    ülesanne

--- MeetodJaCastimine/ülesanded/kiirdete telje/kiirdete telje/Program.cs
+++ MeetodJaCastimine/ülesanded/kiirdete telje/kiirdete telje/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.ComponentModel.Design;
+using System.Security.Cryptography.X509Certificates;
 
 namespace kiirdete_telje
 {
@@ -14,40 +15,84 @@ namespace kiirdete_telje
             float addevalue = juhuarv.Next(75, 299) / 100;
 
             //kirjuta program millel on kaks meetodit
-            string kasutajanimi = "";
-            string parool = "";
+            Console.WriteLine("Tere tulemast toidelljase");
+            Console.WriteLine("Palun logi sisse logimiseks");
+            bool login = UserLogin();
+            if (login == false)
+            {
+                Console.WriteLine("Toidu tellimiseks sa pead olema sisselogitud");
+            }
+            else
+            {
+                Console.WriteLine("Sisesta toit mis osta tahad kui tahad lõpetada kirjuta \nenam ei oloe\n");
+                string Lõpp = "";
+                while (Lõpp != "enam ei ole") ;
+            }
+            {
+                Console.WriteLine("Sisesta järgmine ost");
+                string sisestus = Console.ReadLine();
 
+            }
 
             //UserLogin() ja FoodOrder()
+
+
+
+
             //Userlogin() küsib kasutajalt tema nime ja parooli küsimused on tsüklis ja parooli küsimine peab olema alfanureemiline aga vahemikus 8 tähemärki ja 12 tähemärki pikk.
             // siis kui kasutajanimi ja parool on õige main programmis küsitakse kasutajalt toiduasju mida ta osta tahab
             // kasutaja soovitud asjad hoitakse listis kui rohkem pole ütle kasutajale et ta kirjutaks "tellimuse lõpp"
             // FoodOrder() arvutab kokku kui palju ostukorv maksma läheb. iga asi on 1.5+ mingi random float. tsükkel käib niikaua kuni rohkem asju ei ole.
 
         }
- public static string UserLogin(string kasutajanimi, string kasutajaparool)
-
+        bool login  UserLogin()
         {
-         while (kasutajanimi == "")
+            string kasutajanimi = "";
+            string kasutajaparool = "";
+            while (kasutajanimi == "")
             {
                 Console.WriteLine("Mis on sinu kasutajanimi");
                 kasutajanimi = Console.ReadLine();
+                if (kasutajanimi != "nipitiri") ;
+                {
+                    Console.WriteLine("Kasutajanimi on vale");
+                }
                 {
                     while (kasutajaparool == "") ;
                 }
                 Console.WriteLine("Mis on sinu kasutajaparool");
                 kasutajaparool = Console.ReadLine();
-            }
-            if (kasutajanimi == "Mihhail") ;
-            {
-                Console.WriteLine("Kasutajanimi on vale");
-            }
-            if (kasutajaparool == "1234") ;
-            {
-                Console.WriteLine("Kasutajaparool on vale");
-            }
-        
+                if (kasutajaparool != "LALALA") ;
+                Console.WriteLine("See parool on vale");
+                {
+                    Console.WriteLine("Sisesta parool mis on pikem kui 12 tähe-märki");
+                }
+                if (kasutajaparool.Length < 12) ;
+                {
+                    {
+                        Console.WriteLine("Sisesta parool mis on lühem kui 8 tähe-märki");
+                    }
+                    {
+                        if (kasutajaparool.Length > 8) ;
+                    }
+                    return true;
 
+
+
+
+
+
+
+
+
+
+
+
+                }
+               
+    
+
+            }
         }
     }
 }
