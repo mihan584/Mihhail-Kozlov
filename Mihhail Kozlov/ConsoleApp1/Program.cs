8df5af7207c692cbe4030f5fb632e5466b5e1745
Opilane TTHK <opilane@tthk.ee>
2026-01-15 11:37:23 +0200

    sdf

---
+++ Mihhail Kozlov/ConsoleApp1/Program.cs
@@ -0,0 +1,156 @@
+﻿//Console.WriteLine("tere kasutaja mis on sinu kasutajanimi?");
+//string kasutajanimi = "";
+//do
+//{
+//    Console.WriteLine("palun sisesta oma kasutajanimi");
+//    kasutajanimi = Console.ReadLine();
+
+//} while (kasutajanimi == "mihhail");
+
+//string kasutajasisestus = "";
+//{
+//List<float> nimi = new List<float>();
+//}
+
+//while (kasutajasisestus != "olen sisestanud nime") ;
+
+
+
+
+
+Console.WriteLine("Palun sisesta oma kasutaja nimi");
+string kasutajaNimi = "";
+do
+{
+    Console.WriteLine("Palun sisesta oma kasutajanimi");
+    kasutajaNimi = Console.ReadLine();
+}
+while (kasutajaNimi != "mihhail");
+if (kasutajaNimi == "mihhail") ;
+Console.WriteLine("tere sisesta neljakohalist parooli mis on suurem kui 1000 ja väiksem kui 9999");
+
+int kasutajaparool = 0;
+double kasutajapaool = double.Parse(Console.ReadLine());
+
+{ while (kasutajaparool < 1000 && kasutajapaool > 9999) ; }
+Console.WriteLine("parool sobib");
+{
+}
+
+   
+   
+    
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Console.WriteLine("Palun sisesta oma kasutaja nimi");
+//string kasutajaNimi = "";
+//do
+//{
+//    Console.WriteLine("Palun sisesta oma kasutajanimi");
+//    kasutajaNimi = Console.ReadLine();
+//}
+//while (kasutajaNimi != "user 1");
+//if (kasutajaNimi == "user 1")
+//{
+//    int ruuduSuurus = 0;
+
+//    do
+//    {
+//        Console.WriteLine("Kui suurt ruutu sa tahad?");
+//        ruuduSuurus = int.Parse(Console.ReadLine());
+
+//    } while (ruuduSuurus < 0 && ruuduSuurus > 20);
+//    char reaKujund = 'o';
+//    string üksRida = "";
+//    int tsükliMuutuja = ruuduSuurus;
+//    do
+//    {
+//        üksRida += "_" + reaKujund;
+//        tsükliMuutuja -= 1;
+//    } while (tsükliMuutuja != 0);
+//    tsükliMuutuja = ruuduSuurus;
+//    do
+//    {
+//        Console.WriteLine(üksRida);
+//        tsükliMuutuja -= 1;
+//    } while (tsükliMuutuja != 0);
+//    Console.WriteLine($"Palun, siin on sinu ruut, suurusega {ruuduSuurus} {ruuduSuurus}");
+//}
---
+++ Mihhail Kozlov/tingimuslausetöö/Program.cs
@@ -0,0 +1,43 @@
+﻿//namespace tingimuslausetöö
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello, World!");
+//        }
+//    }
+//}
+
+
+
+// kirjuta programm mis küsib tema nime
+//using System.ComponentModel.Design;
+
+//Console.WriteLine("Tere! Mis on sinu kasutajanimi");
+//string kasutajanimi = "mihhail";
+//do
+//{
+//    Console.WriteLine("Palun sisesta oma kasutajanimi");
+//    {
+//        while (kasutajanimi == "mihhail") ;
+//        if (kasutajanimi != "mihhail")
+//            Console.WriteLine("Nimi on õige");
+//        else;
+
+
+
+
+
+//    }
+
+//}
+
+
+
+
+using System.Runtime.Serialization.Json;
+
+Console.WriteLine("mis on sinu kasutajanimi?");
+
+    
